041f22f1d42d1d9c183ab40bba7174fe31b2176b
Seth Uschuk <seth@hypercolordigital.com>
2023-02-10 13:28:26 -0600

    fixed imports for controller interface

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,7 +1,11 @@
-import {NextFunction} from 'express';
+import {Request, Response, NextFunction} from 'express';
 
-export type IControllerType = new (req: Request, res: Response, next: NextFunction) => any;
-
+export interface IController {
+  request: Request,
+  response: Response
+  start(req: Request, res: Response): Promise<any>,
+  handleRequest(): Promise<any>,
+}
 export interface IControllerDocumentation {
   summary?: string
   description?: string
@@ -9,19 +13,17 @@ export interface IControllerDocumentation {
   query?: {[key: string]: any}
   response?: {[key: string]: any}
 }
-
 export interface IRouterItem {
   path: string,
   verb: string,
   controller: IControllerType | undefined
 }
-
 export interface IRouter {
   routes: Array<IRouterItem>
 }
-
 export interface IMountedRoute {
   path: string,
   verb: string,
   controller: IControllerType | undefined
 }
+export type IControllerType = new (req: Request, res: Response, next: NextFunction) => IController;
